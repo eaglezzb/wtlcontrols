97c96acf2430873f70fa26ac9815eede6048a0fc
zhangyinquan <zhangyinquan@0feb242a-2539-11de-a0d7-251e5865a1c7>
2009-04-27 00:10:37 +0000

    Added use QPainter with MFC based application

---
+++ QtMFCDraw/QtMFCDraw.h
@@ -0,0 +1,32 @@
+
+// QtMFCDraw.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CQtMFCDrawApp:
+// 有关此类的实现，请参阅 QtMFCDraw.cpp
+//
+
+class CQtMFCDrawApp : public CWinAppEx
+{
+public:
+	CQtMFCDrawApp();
+
+// 重写
+	public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CQtMFCDrawApp theApp;
