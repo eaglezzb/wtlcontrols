6fee213fa78bb6d07ceea467fdcde7747801511c
zhangyinquan <zhangyinquan@0feb242a-2539-11de-a0d7-251e5865a1c7>
2009-05-19 10:55:28 +0000

    移植QTCore到当前目录

---
+++ gui/core/core.pri
@@ -0,0 +1,59 @@
+# -*-mode:sh-*-
+# Qt Core handling
+
+HEADERS += \
+        core/qalgorithms.h \
+        core/qatomic.h \
+        core/qatomic_arch.h \
+        core/qatomic_windows.h \
+        core/qbasicatomic.h \
+        core/qbitarray.h \
+        core/qbytearray.h \
+        core/qbytearraymatcher.h\ 
+	core/qconfig.h \
+        core/qcontainerfwd.h\
+        core/qfeatures.h \
+        core/qglobal.h \
+        core/qhash.h \
+        core/qiterator.h \
+        core/qline.h \
+        core/qlinkedlist.h\ 
+	core/qlist.h \
+        core/qmap.h \
+        core/qmath.h \
+        core/qnamespace.h \
+        core/qnumeric.h \
+        core/qnumeric_p.h \
+        core/qpair.h \
+        core/qpoint.h\ 
+	core/qrect.h \
+        core/qset.h\
+        core/qsize.h \
+        core/qt_windows.h \
+        core/qtools_p.h \
+        core/qvarlengtharray.h \
+        core/qvector.h 
+
+
+
+SOURCES += \
+        core/qatomic.cpp \
+        core/qbitarray.cpp \
+        core/qbytearray.cpp \
+        core/qbytearraymatcher.cpp \
+        core/qconfig.cpp \
+        core/qglobal.cpp \
+        core/qhash.cpp \
+	core/qline.cpp \
+        core/qlinkedlist.cpp \
+        core/qlistdata.cpp \
+        core/qmalloc.cpp \
+        core/qmap.cpp \
+        core/qnumeric.cpp \
+        core/qpoint.cpp \
+        core/qrect.cpp \
+        core/qsize.cpp \
+	core/qvector.cpp
+
+
+
---
+++ gui/core/qconfig.cpp
@@ -0,0 +1,27 @@
+/* Licensed */
+static const char qt_configure_licensee_str          [512 + 12] = "qt_lcnsuser=Open Source";
+static const char qt_configure_licensed_products_str [512 + 12] = "qt_lcnsprod=OpenSource";
+static const char qt_configure_prefix_path_str       [512 + 12] = "qt_prfxpath=D:\\qt-win-opensource-src-4.5.1";
+static const char qt_configure_documentation_path_str[512 + 12] = "qt_docspath=D:\\qt-win-opensource-src-4.5.1\\doc";
+static const char qt_configure_headers_path_str      [512 + 12] = "qt_hdrspath=D:\\qt-win-opensource-src-4.5.1\\include";
+static const char qt_configure_libraries_path_str    [512 + 12] = "qt_libspath=D:\\qt-win-opensource-src-4.5.1\\lib";
+static const char qt_configure_binaries_path_str     [512 + 12] = "qt_binspath=D:\\qt-win-opensource-src-4.5.1\\bin";
+static const char qt_configure_plugins_path_str      [512 + 12] = "qt_plugpath=D:\\qt-win-opensource-src-4.5.1\\plugins";
+static const char qt_configure_data_path_str         [512 + 12] = "qt_datapath=D:\\qt-win-opensource-src-4.5.1";
+static const char qt_configure_translations_path_str [512 + 12] = "qt_trnspath=D:\\qt-win-opensource-src-4.5.1\\translations";
+static const char qt_configure_examples_path_str     [512 + 12] = "qt_xmplpath=D:\\qt-win-opensource-src-4.5.1\\examples";
+static const char qt_configure_demos_path_str        [512 + 12] = "qt_demopath=D:\\qt-win-opensource-src-4.5.1\\demos";
+/* strlen( "qt_lcnsxxxx" ) == 12 */
+#define QT_CONFIGURE_LICENSEE qt_configure_licensee_str + 12;
+#define QT_CONFIGURE_LICENSED_PRODUCTS qt_configure_licensed_products_str + 12;
+#define QT_CONFIGURE_PREFIX_PATH qt_configure_prefix_path_str + 12;
+#define QT_CONFIGURE_DOCUMENTATION_PATH qt_configure_documentation_path_str + 12;
+#define QT_CONFIGURE_HEADERS_PATH qt_configure_headers_path_str + 12;
+#define QT_CONFIGURE_LIBRARIES_PATH qt_configure_libraries_path_str + 12;
+#define QT_CONFIGURE_BINARIES_PATH qt_configure_binaries_path_str + 12;
+#define QT_CONFIGURE_PLUGINS_PATH qt_configure_plugins_path_str + 12;
+#define QT_CONFIGURE_DATA_PATH qt_configure_data_path_str + 12;
+#define QT_CONFIGURE_TRANSLATIONS_PATH qt_configure_translations_path_str + 12;
+#define QT_CONFIGURE_EXAMPLES_PATH qt_configure_examples_path_str + 12;
+#define QT_CONFIGURE_DEMOS_PATH qt_configure_demos_path_str + 12;
+
